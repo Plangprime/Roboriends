f7b0a8648b5511d93864262733589376166bb506
unknown <plangnanrengshakvwatshak@gmail.com>
2021-10-22 14:26:08 -0700

    commit

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (probs)=> {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
         
-            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+            <img alt='robots' src={`https://robohash.org/${id}?100x100`} />
         
         <div>
             <h1>{name}</h1>
